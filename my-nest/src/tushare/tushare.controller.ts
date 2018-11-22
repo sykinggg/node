9e005f9214397a5fa7b62b2924ffce57fb32649f
sy <yisui@skspruce.net>
2018-11-22 20:59:40 +0800

    tushare 接口ä¸中转

--- my-nest/src/tushare/tushare.controller.ts
+++ my-nest/src/tushare/tushare.controller.ts
@@ -5,8 +5,40 @@ import { async } from 'rxjs/internal/scheduler/async';
 @Controller('tushare')
 export class TushareController {
     constructor(private tushareService: TushareService) {}
+
+    // 股票列表
     @Post('storkBaseUrl')
-    async storkBaseUrl(@Body() request): Promise<any> {
-        return this.tushareService.storkBaseUrl(request);
+    async storkBaseUrl(@Body() body): Promise<any> {
+        return this.tushareService.storkBaseUrl(body);
+    }
+
+    // 交易日历
+    @Post('tradeCalUrl')
+    async tradeCalUrl(@Body() body): Promise<any> {
+        return this.tushareService.tradeCalUrl(body);
+    }
+
+    // 沪深股通成份股
+    @Post('hsConst')
+    async hsConst(@Body() body): Promise<any> {
+        return this.tushareService.hasConst(body);
+    }
+
+    // 股票曾用名
+    @Post('namechange')
+    async namechange(@Body() body): Promise<any> {
+        return this.tushareService.namechange(body);
+    }
+
+    // 上市公司基本信息
+    @Post('stockCompany')
+    async stockCompany(@Body() body): Promise<any> {
+        return this.tushareService.stockCompany(body);
+    }
+
+    // IPO新股列表
+    @Post('newShare')
+    async newShare(@Body() body): Promise<any> {
+        return this.tushareService.newShare(body);
     }
 }
--- my-nest/src/tushare/tushare.service.ts
+++ my-nest/src/tushare/tushare.service.ts
@@ -10,13 +10,46 @@ export class TushareService {
     ) { }
 
     public baseUrl = 'http://api.tushare.pro';
-    public baseOption = {
-        api_name: 'stock_basic',
-        token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
-        params: {
-            list_status: 'L',
+
+    public option = {
+        storkBaseUrl: {
+            api_name: 'stock_basic',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+            params: {
+                list_status: 'L',
+            },
+            fields: '',
         },
-        fields: '',
+        tradeCalUrl: {
+            api_name: 'trade_cal',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+            params: {
+                exchange: 'SSE',
+                start_date: '20180101',
+                end_date: '20181231',
+                is_open: 1,
+            },
+        },
+        hasConstUrl: {
+            api_name: 'hs_const',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+            params: {
+                hs_type: 'SH',
+                is_new: '1',
+            },
+        },
+        namechangeUrl: {
+            api_name: 'namechange',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+        },
+        stockCompanyUrl: {
+            api_name: 'namechange',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+        },
+        newShareUrl: {
+            api_name: 'namechange',
+            token: '304bd4b4830d85adf5bee28ae7cc6fe79a5c72e3022f6ca816b83894',
+        },
     };
     public httpSet = {
         headers: {
@@ -24,21 +57,63 @@ export class TushareService {
         },
     };
 
-    public storkBaseUrl(req) {
-        if (!req) {
-            req = this.baseOption;
+    public storkBaseUrl(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.storkBaseUrl;
         }
-        return this.httpService.post(this.baseUrl, req, this.httpSet).pipe(map(res => {
+        return this.basePost(params);
+    }
+
+    public tradeCalUrl(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.tradeCalUrl;
+        }
+        return this.basePost(params);
+    }
+
+    async hasConst(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.hasConstUrl;
+        }
+        return this.basePost(params);
+    }
+
+    async namechange(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.namechangeUrl;
+        }
+        return this.basePost(params);
+    }
+
+    async stockCompany(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.stockCompanyUrl;
+        }
+        return this.basePost(params);
+    }
+
+    async newShare(params) {
+        if (!params || JSON.stringify(params) === '{}') {
+            params = this.option.newShareUrl;
+        }
+        return this.basePost(params);
+    }
+
+    public basePost(params) {
+        return this.httpService.post(this.baseUrl, params, this.httpSet).pipe(map(res => {
             let returnData = res.data;
-            if (+res.data.code === 0 && res.data.data.items && res.data.data.items.length) {
-                this.setData(req.api_name, res.data.data);
+            if (+returnData.code === 0) {
+                this.setData(params.api_name, returnData.data);
+            } else {
+                this.StockModel.find({ name: params.api_name }).exec().then(resData => {
+                    returnData = resData[0].data;
+                });
             }
-            returnData = this.StockModel.find({ name: req.api_name }).exec();
             return returnData;
         }));
     }
 
-    public setData(type, dataObj?): void {
+    async setData(type, dataObj?): Promise<any> {
         const oldModal = this.StockModel.find({ name: type }).exec();
         oldModal.then(res => {
             if (res && res.length) {
