ba9083e61a66de37771e2a72ce83d1bb535b70e5
sy <yisui@skspruce.net>
2019-01-02 20:33:17 +0800

    支持各种跨域

--- my-nest/src/main.ts
+++ my-nest/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: 'http://localhost:4200',
-    });
+    app.enableCors();
     await app.listen(666);
 }
 bootstrap();
