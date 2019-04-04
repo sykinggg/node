e7928926fc832869b0aa8871f75e4e4cdca33ea9
sy <yisui@skspruce.net>
2019-04-04 16:59:47 +0800

    1

--- my-nest/src/puppeteer/puppeteer.service.ts
+++ my-nest/src/puppeteer/puppeteer.service.ts
@@ -7,48 +7,33 @@ export class PuppeteerService {
     constructor() { }
 
     async pic(data: any): Promise<any> {
-
         const browser = await (puppeteer.launch({ headless: true }));
-
         const page = await browser.newPage();
-
         if (data.http) {
             await page.goto(data.http);
         }
-
         await page.waitFor(data.waitFor || 1000);
-
         await this.autoScroll(page);
-
         const result = await page.screenshot({
             path: '1.png',
             fullPage: true,
         });
-
         await browser.close();
-
         return result;
     }
 
     async pdf(data: any): Promise<any> {
         const browser = await (puppeteer.launch({ headless: true }));
-
         const page = await browser.newPage();
-
         if (data.http) {
             await page.goto(data.http);
         }
-
         await page.waitFor(data.waitFor || 1000);
-
         await page.emulateMedia('screen');
-
         const result = await page.pdf({
             path: './1.pdf',
         });
-
         await browser.close();
-
         return result;
     }
 
